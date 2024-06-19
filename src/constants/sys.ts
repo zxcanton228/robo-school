94c9a086acb3f45cf41bca0855a435c3ae0accb2
Kirill Vegele <kirillvegele10@gmail.com>
2024-06-19 21:25:42 +0300

    fixed IS_DEV

--- src/constants/sys.ts
+++ src/constants/sys.ts
@@ -1,3 +1,3 @@
 export const URL: string = import.meta.env.VITE_URL,
-	IS_DEV: boolean = import.meta.env.VITE_NODE_ENV,
+	IS_DEV: boolean = import.meta.env.VITE_NODE_ENV === "development",
 	IMG_URL: string = IS_DEV ? "/" : `/${URL}/`
